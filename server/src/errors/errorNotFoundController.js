a2315ec932b5131dfec7e57f1d903454db42fb46
Yesica Astarloa <yesica.astarloa@gmail.com>
2023-09-20 07:18:36 -0300

    se añade archivo error-controller

--- server/src/errors/errorNotFoundController.js
+++ server/src/errors/errorNotFoundController.js
@@ -1,2 +1,10 @@
-const errorNotFoundErrorController = () => {};
-export default errorNotFoundErrorController;
+// Función controladora final para el middleware de errores.
+const errorNotFoundController = (err, req, res, next) => {
+    console.error(err);
+
+    res.status(err.httpStatus || 500).send({
+        status: 'error',
+        message: err.message,
+    });
+};
+export default errorNotFoundController;
