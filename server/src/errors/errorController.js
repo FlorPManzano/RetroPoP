0041323381dc478cabf4a1c16f5e4996ebe1f632
Ignacio Castro <castro.ignacio.agustin@gmail.com>
2023-09-20 12:46:37 +0200

    se añade nuevo error 404

--- server/src/errors/errorController.js
+++ server/src/errors/errorController.js
@@ -1,4 +1,12 @@
 // Función controladora final para el middleware de error.
 // eslint-disable-next-line no-unused-vars
-const errorController = () => {};
+const errorController = (err, req, res, next) => {
+    console.error(err);
+
+    res.status(err.httpStatus || 500).send({
+        status: 'error',
+        message: err.message,
+    });
+};
+
 export default errorController;
