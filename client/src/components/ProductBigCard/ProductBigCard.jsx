5209cecaad13c74323078d8688945210e20741a4
Aitor <aitor.cachero@gmail.com>
2023-10-05 17:56:13 +0200

    Css de producto individual

--- client/src/components/ProductBigCard/ProductBigCard.jsx
+++ client/src/components/ProductBigCard/ProductBigCard.jsx
@@ -15,16 +15,20 @@ export default function ProductBigCard({ product }) {
                                 alt="user"
                                 width="50"
                             />
-                            <h3>{product.username}</h3>
+                            <h3 className="h3-bigproduct">
+                                {product.username}
+                            </h3>
                         </div>
                         <div className="product-page__header__user__reviews">
-                            <h3>
+                            <h3 className="h3-bigproduct">
                                 {product.mediaStars} estrellas (
                                 {product.totalReviews} reviews)
                             </h3>
                         </div>
                         <div className="product-page__header__user__creation">
-                            <p>Fecha de creación: {product.createdAt}</p>
+                            <p className="p-product-created">
+                                Fecha de creación: {product.createdAt}
+                            </p>
                         </div>
                     </div>
                 </header>
@@ -37,18 +41,20 @@ export default function ProductBigCard({ product }) {
                 </div>
                 <div className="product-page__info">
                     <div className="product-page__info__name">
-                        <h2>{product.productName}</h2>
-                        <h3>{product.price}</h3>
+                        <h2 className="h3-bigproduct">{product.productName}</h2>
+                        <h3 className="h3-bigproduct">{product.price}</h3>
                     </div>
                     <div className="product-page__info__description">
-                        <p>{product.description}</p>
+                        <p className="p-product-description">
+                            {product.description}
+                        </p>
                     </div>
                 </div>
                 <footer className="product-page__footer">
                     <div className="product-page__footer__state">
-                        <h3>#{product.category}</h3>
-                        <h3>{product.place}</h3>
-                        <h3>{product.state}</h3>
+                        <h3 className="h3-bigproduct">#{product.category}</h3>
+                        <h3 className="h3-bigproduct">{product.place}</h3>
+                        <h3 className="h3-bigproduct">{product.state}</h3>
                     </div>
                     <div className="product-page__footer__buttons">
                         <button className="product-page__footer__buttons__buy">
